6c7e336d72f912d77546e9b2d71d006827006a76
Canada-College-Game-Design-Club <99418646+Canada-College-Game-Design-Club@users.noreply.github.com>
2022-02-11 22:00:42 -0800

    Added PlayerHealth.cs (Look at description)
    Angel is making the health bar for the UI and this will need to interact with that.

---
+++ Programming/PlayerHealth.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    // Plan:
+
+    /*
+     * 1. Make health field and when SOMETHING happens, lower health by 5. (For now, when space is pressed)
+     * 2. Put the "check" in update function, and create UpdateHealthBar() method that does something visual on the screen.
+     * 3. Initialize in Start()
+     * 
+     */
+
+    // Constraints:
+
+    /* 
+     * 1. Health will not be a decimal; it will remain a whole number.
+     * 2. Health will have range of 1-100.
+     * 
+     */
+
+    // Field for starting health.
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setting the starting health to 100
+        health = 100;
+
+        // Printing out the starting health, which should always be 100.
+        Debug.Log("Starting health: " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks if key pressed is space, if it is then run the code inside curly braces.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Decrease health by 5.
+            health -= 5;
+
+            // Print the new health.
+            Debug.Log("Current health: " + health);
+
+            // Call this method to udpate the health bar that is on the screen (UI). I do not have the health bar thing (UI/UX), and Angel is making it so we will run this with the health bar.
+            // UpdateHealthBar();
+        }
+    }
+}
