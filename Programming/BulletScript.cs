6cca3bf09a98f533b794ccb4fa237188f670d42a
Alexis <92596398+QuantumMNKY@users.noreply.github.com>
2022-02-13 00:04:38 -0800

    Add files via upload
    Temporary script for bullets to destroy game objects

---
+++ Programming/BulletScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
