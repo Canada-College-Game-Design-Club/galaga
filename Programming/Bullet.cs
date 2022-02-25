5cc527d2d769a4f534b36e1a3971aec44041146d
Canada-College-Game-Design-Club <99418646+Canada-College-Game-Design-Club@users.noreply.github.com>
2022-02-25 08:51:45 -0800

    Update Bullet.cs

--- Programming/Bullet.cs
+++ Programming/Bullet.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float life = 3;
-
-    void Awake()
-    {
-        Collider powerUp = GameObject.Find("ScrapMetal").GetComponent<BoxCollider>();
-
-        // Check if the object hit is the scrap metal. if it is not the scrap metal than Destroy.
-        if (Vector3.Distance(powerUp.transform.position, gameObject.transform.position) < 5)
-        {
-            Destroy(gameObject, life);//bullet destroy object
-        }
-    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -24,6 +12,11 @@ public class Bullet : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
+        } else if (collision.gameObject.tag == "ScrapMetal")
+        {
+            Debug.Log("Scrap Metal hit");
+            BoxCollider powerUp = GameObject.Find("ScrapMetal").GetComponent<BoxCollider>() as BoxCollider;
+            powerUp.isTrigger = true;
         }
     }
 }
