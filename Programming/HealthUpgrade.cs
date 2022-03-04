883ab0275afc993e7e0d5d6ee0b0c2a002dc0655
Alexis <92596398+QuantumMNKY@users.noreply.github.com>
2022-03-04 12:27:43 -0800

    Update HealthUpgrade.cs

--- Programming/HealthUpgrade.cs
+++ Programming/HealthUpgrade.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthUpgrade : MonoBehaviour
 {
+    public HealthBar healthBar;
+
+
+    public Text test;
+    public bool flag = false;
+
+
+
     // Fields
     int healthToUpgrade; // This is how much the health will go up.
 
@@ -12,7 +21,11 @@ public class HealthUpgrade : MonoBehaviour
     {
         // Setting the starting health to 5.
         healthToUpgrade = 5;
-    }
+
+        test.text = " ";
+        flag = false;
+        
+     }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -25,9 +38,47 @@ public class HealthUpgrade : MonoBehaviour
 
                 // Increases the players health through the other script.
                 PlayerHealth.health = PlayerHealth.health + healthToUpgrade;
+                flag = true;
+                IncreaseHealth();
 
+                StartCoroutine(oneSecond());
+                
+                
                 Debug.Log("New health: " + PlayerHealth.health);
+
             }
         }
+       
+
+
     }
-}
+    public IEnumerator oneSecond()
+    {
+        flag = false;
+        yield return new WaitForSeconds(.25f);
+        IncreaseText();
+
+    }
+
+    void IncreaseHealth()
+    {
+
+        if (flag)
+        {
+            test.text = "+ 5";
+        }
+        PlayerHealth.currentHealth += healthToUpgrade;
+        healthBar.SetHealth(PlayerHealth.currentHealth);
+
+    }
+    void IncreaseText()
+    {
+        flag = false;
+        if (!flag)
+        {
+            test.text = "";
+        }
+
+    }
+
+}   
