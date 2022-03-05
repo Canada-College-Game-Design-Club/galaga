f7a71c217334271bc98f93571b500ea157108323
Alexis <92596398+QuantumMNKY@users.noreply.github.com>
2022-03-04 18:39:15 -0800

    Update HealthBar.cs

--- Programming/HealthBar.cs
+++ Programming/HealthBar.cs
@@ -7,19 +7,19 @@ public class HealthBar : MonoBehaviour
 {
 
 	public Slider slider;
-
+	
 
 	public void SetMaxHealth(int health)
 	{
 		slider.maxValue = health;
 		slider.value = health;
-;
+		
 	}
 
 	public void SetHealth(int health)
 	{
 		slider.value = health;
-	
+
 	}
 
 }
