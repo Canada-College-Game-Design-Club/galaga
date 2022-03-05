312e4603eb2b6aed4228956853e72f3288574a0e
Alexis <92596398+QuantumMNKY@users.noreply.github.com>
2022-03-04 18:36:37 -0800

    Update ShootingBullet.cs

--- Programming/ShootingBullet.cs
+++ Programming/ShootingBullet.cs
@@ -2,66 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShootingBullet : MonoBehaviour
+public class ShootingBullet : RapidFire //ShootingBullet inherets RapidFire Script
 {
-    // Public fields (Specify these through the inspector)
+
     public Transform spawnPoint;
-    public GameObject bulletObject;
+    public GameObject bullet;
     public float speed = 10;
 
     private bool isCoolDown = false;
     private float coolDown = .5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Checking what keys are being pressed and shooting.
-        if ((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.UpArrow)))
-        {
-            // Checking the state of the cooldown.
-            if (!isCoolDown)
-            {
-                // Shooting
-                Shoot();
 
-                // Starting the cooldown
-                StartCoroutine(CoolDown());
+    // Update is called once per frame
+    private void Update()
+    {
+        // inputs for shooting
+        if ((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.UpArrow) || (Input.GetMouseButtonDown(0))))
+        {
+            if (!isCoolDown)//checks if cooldown is false in order to do stuff.
+            {
+
+                if (rapidFirePowerUp) //if the rapid fire powerup was enabled change speed for rapid fire.
+                {
+
+                    coolDown = .1f;
+                    Shoot();
+                    StartCoroutine(CoolDown());
+                }
+                else //if rapidFirePowerUp isn't on set the cooldown back to normal and proceed to normal shots. 
+                {
+                    coolDown = .5f;
+                    Shoot();
+                    StartCoroutine(CoolDown());
+                }
             }
         }
-        if (!isCoolDown && GameObject.Find("ScrapMetal") != null)
-        {
-            BoxCollider powerUp = GameObject.Find("ScrapMetal").GetComponent<BoxCollider>() as BoxCollider;
-            powerUp.isTrigger = false;
-        }
-    }
 
-    // Method for shooting
-    void Shoot()
-    {
-        // Creating the new bullet.
-        var bullet = Instantiate(bulletObject, spawnPoint.position, bulletObject.transform.rotation);
-
-        if (GameObject.Find("ScrapMetal") != null)
+        void Shoot()
         {
-            BoxCollider powerUp = GameObject.Find("ScrapMetal").GetComponent<BoxCollider>() as BoxCollider;
-            powerUp.isTrigger = false;
+            var bullet = Instantiate(this.bullet, spawnPoint.position, this.bullet.transform.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * speed;
         }
 
-        // Using physics compmonent to move the bullet forward.
-        bullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * speed;
-    }
-
-    // Cooldown
-    IEnumerator CoolDown()
-    {
-        if (GameObject.Find("ScrapMetal") != null)
+        //sets cooldown for how fast we can shoot bullet. Cant shoot if cooldown is true.
+        IEnumerator CoolDown()
         {
-            BoxCollider powerUp = GameObject.Find("ScrapMetal").GetComponent<BoxCollider>() as BoxCollider;
-            powerUp.isTrigger = true;
+            isCoolDown = true;
+            yield return new WaitForSeconds(coolDown);
+            isCoolDown = false;
         }
 
-        isCoolDown = true;
-        yield return new WaitForSeconds(coolDown);
-        isCoolDown = false;
+
+
+
+
+
+
     }
 }
