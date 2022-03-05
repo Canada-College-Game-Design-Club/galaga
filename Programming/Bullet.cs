3545748f7aeacdaaa70004dacb9e3b079f61afdf
Alexis <92596398+QuantumMNKY@users.noreply.github.com>
2022-03-04 18:37:01 -0800

    Update Bullet.cs

--- Programming/Bullet.cs
+++ Programming/Bullet.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class BulletScript : MonoBehaviour
 {
-
+   
     void OnCollisionEnter(Collision collision)
     {
-
+        //only destroys something if the tag is Enemy
         if (collision.gameObject.tag == "Enemy")
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
-        } 
+        }
+
     }
 }
